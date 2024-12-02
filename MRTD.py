b331d79a2c3a13a1e8a20e6f3f5c03dcca4ebfac
grahamprimm <grahamprimm@gmail.com>
2024-12-02 17:40:14 -0500

    added query db and decode functions

--- MRTD.py
+++ MRTD.py
@@ -4,8 +4,40 @@ class MRTDProcessor:
         # Define weights for check digit calculation
         self.weights = [7, 3, 1]
 
+    def query_database(self):
+        """
+        Placeholder for database interaction.
+        Should retrieve travel document information fields for encoding.
+        """
+        pass
 
+    def decode_mrz(self, line1: str, line2: str) -> dict:
+        """
+        Decodes the two MRZ strings into respective fields.
 
+        :param line1: The first line of the MRZ.
+        :param line2: The second line of the MRZ.
+        :return: A dictionary containing the decoded fields.
+        """
+        # Adjsuted this line based on the data given in part 3 changing gender to sex, and name to last_name and given_name
+        name_parts = line1[5:].rstrip("<").split("<<")
+        decoded = {
+            "type": line1[:1],
+            "issuing_country": line1[2:5],
+            "last_name": name_parts[0],
+            "given_name": name_parts[1].replace("<", " ") if len(name_parts) > 1 else "",
+            "passport_number": line2[:9],
+            "passport_number_check": int(line2[9]),
+            "country_code": line2[10:13],
+            "birth_date": line2[13:19],
+            "birth_date_check": int(line2[19]),
+            "sex": line2[20],
+            "expiration_date": line2[21:27],
+            "expiration_date_check": int(line2[27]),
+            "personal_number": line2[28:42].rstrip("<"),
+            "personal_number_check": int(line2[-1]),
+        }
+        return decoded
 
     def encode_mrz(self, fields: dict) -> tuple:
         """
