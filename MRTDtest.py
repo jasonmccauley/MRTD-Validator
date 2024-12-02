2666049a082e07b6a1d287d59f8d2e8476946123
Ahmad Chaabane <achaaba1@stevens.edu>
2024-12-02 18:48:22 -0500

    Mutation testing, adding test case to satisfy mutation testing and coverage testing, along with updated coverage score

--- MRTDtest.py
+++ MRTDtest.py
@@ -168,6 +168,34 @@ class Test_MRTD_Processor(unittest.TestCase):
         # We set the epected results to an emtpy array if it passes all the check digits
         self.assertEqual(result, expected_result)
 
+    # Route for testing the validation of the feilds according to the check digits
+    # Added this test case to kill mutants 
+    def test_MRZ_validator_invalid(self):
+        """
+        Test validate_mrz with MRZ lines where some check digits are incorrect.
+        """
+        # Expected results for different mismatched cases
+        expected_result1 = ["Mismatch in birth date check digit.", "Mismatch in expiration date check digit."]
+        expected_result2 = ["Mismatch in passport number check digit.", "Mismatch in personal number check digit."]
+
+        # Example MRZ lines for each case
+        line1_example = "P<UTOERIKSSON<<ANNA<MARIA<<<<<<<<<<<<<<<<<<<"
+        line2_example1 = "L898902C36UTO6908062F1204158ZE184226B<<<<<<<1"  # Incorrect birth date and expiration check digits
+        line2_example2 = "L898902C26UTO7408122F1204159ZE184226B<<<<<<<2"  # Incorrect passport number and personal number check digits
+
+        # Intilize the MRTDProcessor
+        check = MRTDProcessor()
+        #Running it through the function
+        result1 = check.validate_mrz(line1_example, line2_example1)
+        # Matched the results with expected results
+        self.assertEqual(result1, expected_result1)
+
+        # Validate passport number and personal number mismatches
+        # Running the examples through the function 
+        result2 = check.validate_mrz(line1_example, line2_example2)
+        # Matching it with the results
+        self.assertEqual(result2, expected_result2)
+
 
 if __name__ == '__main__':
     unittest.main()
